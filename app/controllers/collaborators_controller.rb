d63a52d036cf018d506b0ee2c65edbbd93d15153
jason@leinberberdev.com <jason@leinbergerdev.com>
2018-01-22 15:42:47 -0600

    collaborators: complete

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,22 +1,34 @@
 class CollaboratorsController < ApplicationController
-  def :new
-    @Collaborator = Collaborator.new
+
+  def new
+    @collaborator = Collaborator.new
   end
 
-  def :create
+  def create
     @wiki = Wiki.find(params[:wiki_id])
     @collaborator = Collaborator.new(wiki_id: @wiki.id, user_id: params[:user_id])
 
     if @collaborator.save
       flash[:notice] = "Collaborator was added to this wiki."
-      redirect_to @wiki
+      redirect_to edit_wiki_path(@wiki)
     else
       flash[:alert] = "Error adding collaborator to wiki.  Please try again."
+      render :show
     end
   end
 
-  def :destroy
+  def destroy
     @wiki = Wiki.find(params[:wiki_id])
+
     @collaborator = Collaborator.find_by(wiki_id: @wiki.id, user_id: params[:user_id])
+
+    if @collaborator.destroy
+      flash[:notice] = "Collaborator's access has been revoked."
+      redirect_to edit_wiki_path
+    else
+      flash[:alert] = "Calloborator was not removed.  Please try again."
+      redirect_to edit_wiki_path
+    end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :collaborators
+  resources :collaborators, only: [:new, :create, :destroy]
 
   resources :wikis
 
